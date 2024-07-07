86fd38fd05e1fcba934461f659f3b6bf40439ab4
Dmitry Poroh <dporoh@pm.me>
2024-07-07 13:48:17 -0700

    Add: variant/overloaded.hpp

---
+++ craftpp/variant/overloaded.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2023-2024 Dmitry Poroh
+// All rights reserved.
+// Distributed under the terms of the MIT License. See the LICENSE file.
+//
+// Code from https://en.cppreference.com/w/cpp/utility/variant/visit example
+// to use lambda-like matching for variants
+//
+//
+
+#pragma once
+
+namespace craftpp::variant {
+
+template<class... Ts>
+struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+}
